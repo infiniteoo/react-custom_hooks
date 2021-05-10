055a6e7d0cd84ac42b772c8e6d53b40cc9fb7068
Troy Dorman <troydorman@gmail.com>
2021-05-10 13:20:43 -0600

    added .env and our giphy api key.  made successful call

--- src/components/Random_V1.jsx
+++ src/components/Random_V1.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+
+// API KEY GIPHY DEVELOPERS KEY
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 
 const Random = () => {
+
+    useEffect(() => {
+        const fetchGif = async () => {
+            const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+            const {data } = await axios.get(url)
+            console.log(data)
+
+        }
+       
+        fetchGif();
+
+
+    }, []);
+
+
     return (
         <>
             <h1>Random</h1>
