40ee8e694ec4052920ac66cacfb6d72976907f6d
Troy Dorman <troydorman@gmail.com>
2021-05-10 13:11:42 -0600

    implemented our Tag and Random components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
 
+import Random from './components/Random_V1'
+import Tag from './components/Tag_V1'
+
+
 import './App.css'
 
 
-const App = () => {
-    return (
-        <div>
-            <h1> Random GIF Application </h1>
+const App = () => (
+    <div>
+        <h1>Random GIF Application</h1>
+        <div className="main-container">
+            <Random/>
+            <Tag/>
         </div>
-    )
-}
+
+    </div>
+)
 
 export default App
